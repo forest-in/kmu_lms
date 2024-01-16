07040e757b79470d7ee286743c941c716c6b170e
Forest In <webin813@gmail.com>
2024-01-16 22:39:54 +0900

    링크 router history -> hash 모드로 수정

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LmsMain from '@/components/lms/contents/LmsMain.vue'
 
 const routes = [
@@ -87,7 +87,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
