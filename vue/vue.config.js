0ebdd4f6f06222555e050958a439a618c7723ccb
Forest In <webin813@gmail.com>
2024-01-16 21:37:57 +0900

    vue file build -- > 배포

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,9 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true ,
-  resolve: {
-    alias: {
-      '@vuepic': '@vuepic/vue-datepicker',
-    },
-  }
-})
+module.exports = {
+  publicPath: './',
+  assetsDir: './',
+}
