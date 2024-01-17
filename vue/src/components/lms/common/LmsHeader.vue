56fa6d7b2bdd15774220e99ab327c91bb30822bf
Forest In <webin813@gmail.com>
2024-01-17 09:28:36 +0900

    링크 수정

--- vue/src/components/lms/common/LmsHeader.vue
+++ vue/src/components/lms/common/LmsHeader.vue
@@ -3,7 +3,7 @@
     <div class="top rel">
       <div class="inner">
         <div class="flex__d__m">
-          <a href="/lms/" class="logo block"><img src="@/assets/lms-img/logo.png" alt="NCCOSS 첨단분야 혁신 융합대학 LMS"></a>
+          <router-link to="/lms/" class="logo block"><img src="@/assets/lms-img/logo.png" alt="NCCOSS 첨단분야 혁신 융합대학 LMS"></router-link>
           <ul class="t-menu flex__m tc">
             <li><a href="" class="block rel"><span class="material-symbols-outlined">notifications</span><i class="abs block tc">1</i><p>알림</p></a></li>
             <li><a href="" class="block rel"><span class="material-symbols-outlined">mail</span><i class="abs block tc">2</i><p>쪽지</p></a></li>
